71c6c3ca389b5723aca8c2e8323188c4567f6e13
BENSTOH <benson.toh@daimler.com>
2021-04-04 20:35:34 +0800

    lint: remove unused code

--- src/index.js
+++ src/index.js
@@ -2,26 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import storage from './utils/storage'
-import { generateRandomId } from '../src/utils/helper'
-
-const alice = {
-  uid: generateRandomId(),
-  name: 'Alice',
-  balance: 0
-}
-
-const bob = {
-  uid: generateRandomId(),
-  name: 'Bob',
-  balance: 0
-}
-
-const jane = {
-  uid: generateRandomId(),
-  name: 'Jane',
-  balance: 100
-}
-
 
 function hasStorage() {
   return storage.getCurrentLogin() && storage.getDebtRecords() && storage.getUsers()
